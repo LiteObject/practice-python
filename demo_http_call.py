9c3388e92ea91e82b65a8276fc2c9cc41fd13f99
LiteObject <Lite.Object@gmail.com>
2023-02-28 22:32:37 -0600

    added json formatter

--- demo_http_call.py
+++ demo_http_call.py
@@ -1,16 +1,29 @@
 """
-This script demonstrate HTTP call
+In this example, the script imports the `urllib.request` module to 
+make an HTTP GET request to the URL specified in the url variable. 
+The response is stored in the response variable, and the read() 
+method is used to retrieve the response data. Finally, the script 
+prints the response data to the console using the print() function.
+
+Note that you may need to install additional libraries, such as requests, 
+if you need to make more advanced HTTP requests or handle HTTP responses 
+in a specific way.
 """
 
-import http.client
+import urllib.request
+import json
 
-connection = http.client.HTTPConnection('www.google.com', 80, timeout=10)
-connection.request('GET', '/')
+# Set the URL to make a request to
+URL = "https://official-joke-api.appspot.com/random_joke"
 
-response = connection.getresponse()
+# Send an HTTP GET request to the URL
+response = urllib.request.urlopen(URL)
 
-print(response.status, response.reason)
+# Read the response and print it
 data = response.read()
-print(data)
+#print(data)
 
-connection.close()
+# Python Pretty Print JSON String
+json_object = json.loads(data)
+json_formatted_str = json.dumps(json_object, indent=2)
+print(json_formatted_str)
